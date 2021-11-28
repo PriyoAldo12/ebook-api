1a8935a5ea910f3275a16ed70c9d21914d7c016b
Priyo Aldo <priyoaldo@Aldo.local>
2021-11-28 11:26:41 +0700

    Miggration and Model

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('me', 'App\Http\Controllers\AuthController');
+
+Route::resource('book', BookController::class)->except('edit', 'create');
